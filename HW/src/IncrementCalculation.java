0378b1642653a71ca5945157e75cf9786b486d3f
vd50angi <adevega13@gmail.com>
2021-09-26 18:12:10 -0400

    IncrementCalc

--- HW/src/IncrementCalculation.java
+++ HW/src/IncrementCalculation.java
@@ -13,27 +13,28 @@ public class IncrementCalculation {
 		System.out.println("Enter the Performance Appraisal Rating");
 		double appraisalRating = sc.nextDouble();
 		
-		if (salary > 0 || appraisalRating < 0.1 || appraisalRating > 0.3) {
-			
+		if (salary <= 0 || appraisalRating < 5 ) {
+		
+	//if salary is 0 or negative and appraisal rating  isa not range of 1 to 5 display 	Invalid input
 			System.out.println("Invalid Input");
 						
 		}
 		
 		else if (appraisalRating >= 1 && appraisalRating  >=3) {
 			salary = (int) (salary + (0.1 * salary));
-			
+		//	//if the appraisal rating  is between 1 & 3 , the salary increment is 10%	
 			System.out.println(salary);
 		}
 		
 		else if (appraisalRating > 3.1 && appraisalRating >= 4) {
 			salary = (int) (salary + ( 0.25 * salary));
-			
+	// 		//if the appraisal rating  is between 3.1 & 4 , the salary increment is 25%	
 			System.out.println(salary);
 		}
 		
 		else if (appraisalRating >= 4.1 && appraisalRating >=5) {
 			salary = (int) (salary + (0.3 *salary));
-			
+			//if the appraisal rating  is between 4.1 & 5 , the salary increment is 30%	
 			System.out.println(salary);
 			
 		}
@@ -42,13 +43,12 @@ public class IncrementCalculation {
 			
 			
 			
-		}
 		
 		
 		
 		
-				
-	}
+		
+	
 		
 	
 	
